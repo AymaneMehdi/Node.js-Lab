8a865ac7871de0e596334e43b0594185c0a22b64
Aymane Mehdi <yorubornin1999@gmail.com>
2026-05-12 20:26:43 +0100

    os.userInfo()

---
+++ os/os.userInfo.js
@@ -0,0 +1,15 @@
+const os = require('os');
+
+console.log(os.userInfo());
+
+// Output
+
+```
+{
+  uid: -1,
+  gid: -1,
+  username: 'AymaneMehdi',
+  homedir: 'C:\\Users\\AymaneMehdi',
+  shell: null
+}
+```
