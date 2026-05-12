13ffb6eb6e2c660e80838bddaf6678b5f378776c
Aymane Mehdi <yorubornin1999@gmail.com>
2026-05-12 20:29:11 +0100

    os.networkInterfaces()

---
+++ os/os.networkInterfaces.js
@@ -0,0 +1,18 @@
+const os = require('os');
+
+console.log(os.networkInterfaces());
+
+// Output:
+
+```
+'Wi-Fi': [
+    {
+      address: '',
+      netmask: '',
+      family: '',
+      mac: '',
+      internal: ,
+      cidr: '',
+      scopeid: 
+    },
+```
