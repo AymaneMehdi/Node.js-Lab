1656a83b48e3693d4fe3e68e64149b69fdadb1f6
Aymane Mehdi <yorubornin1999@gmail.com>
2026-05-12 19:05:14 +0100

    Add __dirname.js and __filename.js files with directory and file path

---
+++ __filename__dirname/__dirname.js
@@ -0,0 +1,4 @@
+// Prints the current directory
+console.log('Directory:', __dirname);
+
+// Directory: C:\Users\AymaneMehdi\Node.js-Lab\__filename__dirname
---
+++ __filename__dirname/__filename.js
@@ -0,0 +1,4 @@
+// Prints the current file path
+console.log('File:', __filename);
+
+// File: C:\Users\AymaneMehdi\Node.js-Lab\__filename__dirname\__filename.js
