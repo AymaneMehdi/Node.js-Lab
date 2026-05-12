f7542ca5745fd3cfd22620933ca7a89b07f49384
Aymane Mehdi <yorubornin1999@gmail.com>
2026-05-12 20:52:59 +0100

    fs.readFile()

---
+++ fs/fs.readFile.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+fs.readFile('test.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(data);
+});
+
+// Output:
+// Hello, World!
