966ecf60aaeb4ba80619a5ad3af88db4339a8043
Aymane Mehdi <yorubornin1999@gmail.com>
2026-05-12 20:03:32 +0100

    os.cpus()

---
+++ os/os.cpus.js
@@ -0,0 +1,18 @@
+const os = require('os');
+
+console.log(os.cpus());
+
+// Output : 
+
+```{
+    model: 'AMD Ryzen 9 6900HX with Radeon Graphics',
+    speed: 3294,
+    times: {
+      user: 7031343,
+      nice: 0,
+      sys: 9370796,
+      idle: 52312515,
+      irq: 1311796
+    }
+  },
+```
